59808463be3a5aeabb5c3a0b4b37f9487571945f
dashingshakeel <shkeelrauf@gmail.com>
2017-01-26 17:45:03 +0500

    Changes for creating posts with token_auth

--- api_client.rb
+++ api_client.rb
@@ -1,17 +1,15 @@
 require "faraday"
-require 'oj'
+
 
 client = Faraday.new(url: 'http://localhost:3000') do |config|
-  config.adapter Faraday.default_adapter
-
-  
+  config.adapter  Faraday.default_adapter
+  config.token_auth('60656371432bfc6b7cffcf1f051acdf8')
+end
+ 
+response = client.post do |req|
+  req.url '/api/v1/posts'
+  req.headers['Content-Type'] = 'application/json'
+  req.body = '{ "post": {"title": "Tislassaasasassaasasasasas", "body": "Text"} }'
 end
 
-response =  client.post do |req|
-  req.url 'api/v1/users'
-  req.headers['Content-Type']='application/json'
-  req.body ='{"user" :{"name": "te1st"}}'
-  
-end
-puts Oj.load(response.body)
 puts response.status
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,11 +1,34 @@
 module Api
     module V1
         class PostsController < ApplicationController
+          include ActionController::HttpAuthentication::Token::ControllerMethods
+          before_action :authenticate, only: [:create, :destroy]
           def index
             @posts = Post.order('created_at DESC')
             
             render json: @posts
           end
+
+          def create
+            @post = @user.posts.new(post_params)
+            if @post.save
+                render json: @post, status: :created
+            else
+                render json: @post.errors, status: :unprocessable_entity
+            end
+          end
+
+          private 
+           def authenticate
+            authenticate_or_request_with_http_token do |token,options|
+              @user = User.find_by(token: token)
+              
+            end
+             
+            def post_params
+              params.require(:post).permit(:title,:body)
+            end
+           end
         end
     end
 end
