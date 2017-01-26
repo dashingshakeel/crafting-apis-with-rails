c27b5455f978f6016e2c1a8b7e1ddd3b2bfc0245
dashingshakeel <shkeelrauf@gmail.com>
2017-01-26 17:14:39 +0500

    Create req of user by Faradar gem

---
+++ api_client.rb
@@ -0,0 +1,17 @@
+require "faraday"
+require 'oj'
+
+client = Faraday.new(url: 'http://localhost:3000') do |config|
+  config.adapter Faraday.default_adapter
+
+  
+end
+
+response =  client.post do |req|
+  req.url 'api/v1/users'
+  req.headers['Content-Type']='application/json'
+  req.body ='{"user" :{"name": "te1st"}}'
+  
+end
+puts Oj.load(response.body)
+puts response.status
